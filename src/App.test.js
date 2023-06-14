eef43c4dbf9a62fc92c92d695b367106961a3ceb
Deepika Chippishetti <deepika.chippishetti.@everest.engineering>
2023-06-14 11:28:00 +0530

    Uneccessary part is removed

--- src/App.test.js
+++ src/App.test.js
@@ -3,9 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
-
   const element = screen.getByRole("heading");
     expect(element).toBeInTheDocument();
   });
