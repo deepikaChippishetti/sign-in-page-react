1e3e8bf70b579216ed08a8ae36bfa4720d4c5f73
Deepika Chippishetti <deepika.chippishetti.@everest.engineering>
2023-06-14 10:55:49 +0530

    Sign-in-page is created

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import Details from "./Details";
+import { Action } from "./data";
+const button = {
+  cancel: "Cancel",
+  create: "Create User",
+};
+export default function App() {
+  return (
+    <div className="sign-in-page">
+      <h1>Sign-in user</h1>
+      <p>Please fill the below details</p>
+      <>
+        <Details />
+        <br></br>
+        <div id="action">
+          <Action btn={button.cancel} />
+          <Action btn={button.create}/>
+        </div>
+      </>
+    </div>
+  );
+}
