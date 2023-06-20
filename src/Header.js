9378450147ed0dfee50e2350a1d5ccf9697db42b
Deepika Chippishetti <deepika.chippishetti.@everest.engineering>
2023-06-20 09:06:33 +0530

    Title of the page and tag line are added in heade component

---
+++ src/Header.js
@@ -0,0 +1,7 @@
+   export function Header(){
+    return(
+   <div id="sign-in-page" data-testid="div">
+        <h1>Sign-in user</h1>
+        <p>Please fill the below details</p>
+    </div>
+   )};
