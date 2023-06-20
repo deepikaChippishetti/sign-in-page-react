48b84898dc986aca74606afe01cf4fc2b827cdc5
Deepika Chippishetti <deepika.chippishetti.@everest.engineering>
2023-06-20 09:23:38 +0530

    Testing done with getByText for first name, last name, mail and phone number

--- src/App.test.js
+++ src/App.test.js
@@ -11,3 +11,28 @@ test('test case for heading', () => {
     const inputelement = screen.getByTestId("div");
       expect(inputelement).toBeInTheDocument();
     });
+    test('test case for div', () => {
+      render(<App />);
+      const inputelement = screen.getByTestId("div");
+        expect(inputelement).toBeInTheDocument();
+      });
+      test("renders form firstname", () => {
+        render(<App />);
+        const name = screen.getByText("First Name");
+        expect(name).toBeInTheDocument();
+      });
+      test("renders form lastname", () => {
+        render(<App />);
+        const name = screen.getByText("Last Name");
+        expect(name).toBeInTheDocument();
+      });
+      test("renders form email", () => {
+        render(<App />);
+        const name = screen.getByText("Email");
+        expect(name).toBeInTheDocument();
+      });
+      test("renders form phonenumber", () => {
+        render(<App />);
+        const name = screen.getByText("Phone Number");
+        expect(name).toBeInTheDocument();
+      });
